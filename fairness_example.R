428eb93b688c8547af6dcdabe05050760e02023f
Juha Karvanen <67104589+JuhaKarvanen@users.noreply.github.com>
2024-03-27 19:20:43 +0200

    Updated for arXiv v3

--- fairness_example.R
+++ fairness_example.R
@@ -1,13 +1,13 @@
 # Filename: fairness_example.R
 # Author: Juha Karvanen
-# Date: 2023-06-10 (updated 2024-03-14)
+# Date: 2024-03-14
 # Description: Reproduces the example in Section 5 of paper
-# J. Karvanen, S. Tikka, M. Vihola (2023) Simulating counterfactuals. 
+# J. Karvanen, S. Tikka, M. Vihola (2023) Simulating counterfactuals, 
 # arXiv:2306.15328, https://arxiv.org/pdf/2306.15328
 
-install.packages("R6causal_0.8.0.tar.gz", repos = NULL, type = "source")
+
 library(R6causal)
-stopifnot(packageVersion("R6causal")=="0.8.0")
+stopifnot(packageVersion("R6causal")>="0.8.3")
 library(data.table)
 #devtools::install_github("https://github.com/dmlc/xgboost/", subdir="R-package")
 library(xgboost)
@@ -85,49 +85,54 @@ credit <- SCM$new("credit scoring",
                   vflist = list(
                     age = "u_age : round(18 + 60*pnorm(u_age)) ",
                     ethnicity = "u_ethnicity : rcateg(c(0.75,0.15,0.10), pnorm(u_ethnicity), softmax = FALSE)",
-                    marital = "u_marital, u2, age: 
-                                rcateg(cbind(1, 
-                                         0.5 + 0.1*(age + 3*u2 - 22), 
-                                         0 + 0.2*(age + 2*u2 - 29)), 
-                                      pnorm(u_marital), softmax = TRUE)",
                     gender = "u_gender: as.numeric(u_gender > 0)",
+                    marital = "u_marital, u2, age: 
+                                rcateg(cbind(-1 + 0.25*(42 - pmin(age,42)), 
+                                         pmax(0.2,1 + 3*u2), 
+                                         0 + 0.03*(age + 12*(age/78)^2+ u2 - 32)), 
+                                      pnorm(u_marital), softmax = TRUE)", #1 single, 2 married or cohabiting, 3 divorced or widowed
                     education = "u_education, u4, u2, gender, age, ethnicity : 
-                                  rcateg(cbind(-gender, 
-                                  u4 + u2 - gender + 0.1*age + as.numeric(ethnicity == 1),
-                                  2*u4 + 2*u2 - 2*gender + 0.2*(age - 22) + 2*as.numeric(ethnicity == 1),
-                                  3*u4 + 3*u2 - 3*gender + 0.3*(age - 28) + 3*as.numeric(ethnicity == 1)),
-                                   pnorm(u_education), softmax = TRUE, asfactor = FALSE)",
+                                  rcateg(cbind(-1 - u2 - u4 + as.numeric(ethnicity == 2) + as.numeric(ethnicity == 2)*gender, 
+                                  4 + u4 + u2 - gender + 0.1*(pmin(10,abs(30-age))) + as.numeric(ethnicity == 1), 
+                                  2*u4 + 2*u2 - 2*gender + 0.6*(pmin(age,30) - 22) + 2*as.numeric(ethnicity == 1), 
+                                  0.6*(3*u4 + 3*u2 + 30/(6 + sqrt(abs(40-age)) + gender) + 3*pmin(0,age - 25) + pmin(0,age - 30) + 3*as.numeric(ethnicity == 1))), 
+                                   pnorm(u_education), softmax = TRUE, asfactor = FALSE)", # 1 primary, 2 secondary, 3 tertiary, 4 doctorate
                     children = "u_children, age, ethnicity, education, marital: 
                                  qpois( p = pnorm(u_children),
-                                 lambda = pmax(0.2, -0.2 * (ethnicity == 1) + 0.5*(marital == 3) +
-                                 as.numeric(age < 45)*(age - 18)/13 + 2*as.numeric(age >= 45)))",
-                    job = "u_job, u4, u2, gender, ethnicity, education, children, age:
-                            rcateg(cbind( 0.2*(ethnicity != 1) + (education <= 1),
-                            u2 + u4 + (education >= 2),
-                            u2 + u4 + (education == 3) + 0.01*(age - 28)), 
-                            pnorm(u_job), softmax = TRUE)",
+                                 lambda = pmax(0.2, -0.3 * (ethnicity == 1) + 0.5*(marital == 2) + 0.5*(marital == 3) +
+                                 0.1*education - 0.4*as.numeric(education == 1) +
+                                 as.numeric(age < 45)*(age - 18 - education)/13 + 2*as.numeric(age >= 45)))",
+                    job = "u_job, u4, u2, gender, ethnicity, education, age:
+                            rcateg(cbind( -3 + 0.2*(ethnicity != 1) + (education == 1) + 1.4*pmax(0,22-age) + 1*pmax(0,pmin(4,28-age)), 
+                            u2 + u4 + (education >= 2), 
+                            -u2 - u4 +  pmin(8,pmax(-8,age - 63) - education + gender)),  
+                            pnorm(u_job), softmax = TRUE)", #1 not working and not retired, 2 working, 3 retired
+                    length_of_employment = "u_length_of_employment, job, age, education:
+                                    (job==1)*0 + (job==3)*0 +
+                                    (job==2) * pmax(0, pmin(age - 18 - 3*education + 2*u_length_of_employment, 
+                                                0.3*(age - 18)  + 6*u_length_of_employment))",
                     income = "u_income, u4, job, education, length_of_employment, age:
-                              pmax(0, -10000 + 25000*as.numeric(job) + 5000*u4 + 10000*u_income + 2000*education + 
-                              200*length_of_employment + 100*(age - 35))",
-                    housing = "u_housing, u3, u5, marital, children, age, education, income:
-                                rcateg(cbind(7, 0.1*( u3 + u5 + as.numeric(marital) + children + age + education + income/10000)), 
-                                pnorm(u_housing), softmax = TRUE)", # 1 rent, 2 own
+                              (job == 1) * pmax(0,10000 + 2000*u4 + 5000*u_income) +
+                              (job == 2) * pmax(10000 + 1000*u_income, 25000 + 5000*u4 + 10000*qt(pnorm(u_income),df=5) + 2000*education + 
+                              200*length_of_employment + 200000/(10 + sqrt(abs(58-age))))  +
+                              (job == 3) * pmax(0,20000 + 5000*u4 + 6000*u_income + 2000*education)",
                     address = "u_address, u5, marital, children, ethnicity, age, income:
                                 round( pmin(10,pmax(1, -4 + u_address + u5 + as.numeric(marital) + 
-                                      (ethnicity == 1) + age/30 + income/10000 )))",
+                                      (ethnicity == 1) + age/30 + income/10000 )))", # 1,2,...,10
+                    housing = "u_housing, u3, u5, marital, children, age, education, income:
+                                rcateg(cbind(7, 0.18*( 10 + u3 + u5 + as.numeric(marital) + children + age/3 + education + income/10000)), 
+                                pnorm(u_housing), softmax = TRUE)", # 1 rent, 2 own
                     savings = "u_savings, u1, u3, marital, gender, ethnicity, children, education, income, age:
-                                pmin(900000 + 10000*exp(u_savings), pmax(0, -5000 + exp(u_savings + u1 + u3 + income/10000 + age/10)))",
-                    length_of_employment = "u_length_of_employment, age, education:
-                                    pmax(0, pmin(age - 18 - 3*education + u_length_of_employment, 
-                                                0.3*(age - 18)  + 3*u_length_of_employment))",
-                    credit_amount = "u_credit_amount, u1, income, age, housing, job, savings, marital, children:
-                     pmax(5000, 110000 - 4000*abs(age - 40) + 2*(income - 30000) - 20000*as.numeric(housing) + 10000*as.numeric(job) +
+                                0.2*pmax(0, -5000 + (qgamma(pnorm(u_savings+ u1 + u3), shape = 3, scale = 1)) * 
+                                (5000 + 0.05*income*(age-17) - 20000/(1+sqrt(abs(age-27)))  + 2000*(education-2) + 2000*(ethnicity==3) + 2000*(marital==2) - 2000*children))",
+                   credit_amount = "u_credit_amount, u1, income, age, housing, job, savings, marital, children:
+                     pmax(5000, 110000 - 4000*abs(age - 40) + 2*(income - 30000) - 20000*as.numeric(housing) + 10000*as.numeric(job==2) +
                      - 0.2*savings + 20000*as.numeric(marital) + 10000*children + 40000*u_credit_amount + 10000*u1)",
                     default = "u_default, u1, length_of_employment, housing, income, age, savings, education, job, 
                     credit_amount, ethnicity:
-                               as.numeric(qlogis(p = pnorm(u_default), 2.2*income + 10000*education + 
-                               10000*as.numeric(job) + 3000*length_of_employment + 10000*u1 - 0.7*credit_amount +
-                                5000*as.numeric(ethnicity), scale = 5000) < 0)" # 1 defaults
+                               as.numeric(qlogis(p = pnorm(u_default), -4000 + 2.2*income + 10000*education + 
+                               10000*as.numeric(job==2) + 3000*length_of_employment + 10000*u1 - 0.7*credit_amount +
+                                5000*as.numeric(ethnicity) + 500*age + 10000*(as.numeric(housing) - 1), scale = 5000) < 0)" # 0 no default,  1 default
                   )
 )
 
@@ -135,6 +140,52 @@ credit <- SCM$new("credit scoring",
 trainingdata <- credit$simulate(10000, return_simdata = TRUE)
 print(summary(trainingdata))
 testdata <- credit$simulate(n=2000, return_simdata = TRUE)
+
+# Descriptive statistics for trainingdata
+with(trainingdata,table(age))
+with(trainingdata,table(ethnicity))
+with(trainingdata,table(gender))
+with(trainingdata,prop.table(table(age,marital), margin = 1))
+with(trainingdata,prop.table(table(age,education), margin = 1))
+with(trainingdata,prop.table(table(age,children), margin = 1))
+with(trainingdata,prop.table(table(marital,children), margin = 1))
+with(trainingdata,prop.table(table(education,children), margin = 1))
+with(trainingdata,prop.table(table(age,job), margin = 1))
+with(trainingdata,prop.table(table(education,job), margin = 1))
+ddply(subset(trainingdata,job==2),.(age),summarize, meanlength = mean(length_of_employment), maxlength = max(length_of_employment))
+ddply(trainingdata,.(job,age),summarize, meanincome = mean(income), minincome = min(income), maxincome = max(income))
+with(trainingdata,table(address))
+with(trainingdata,prop.table(table(marital,address), margin = 1))
+with(trainingdata,prop.table(table(children,address), margin = 1))
+with(trainingdata,prop.table(table(ethnicity,address), margin = 1))
+ddply(trainingdata,.(address),summarize, meanincome = mean(income), minincome = min(income), maxincome = max(income))
+with(trainingdata,table(housing))
+with(trainingdata,prop.table(table(address,housing), margin = 1))
+with(trainingdata,prop.table(table(marital,housing), margin = 1))
+with(trainingdata,prop.table(table(age,housing), margin = 1))
+ddply(trainingdata,.(age),summarize, meansavings = mean(savings), mediansavings = median(savings), minsavings = min(savings), maxsavings = max(savings))
+ddply(trainingdata,.(ethnicity),summarize, meansavings = mean(savings), mediansavings = median(savings), minsavings = min(savings), maxsavings = max(savings))
+ddply(trainingdata,.(marital),summarize, meansavings = mean(savings), mediansavings = median(savings), minsavings = min(savings), maxsavings = max(savings))
+ddply(trainingdata,.(education),summarize, meansavings = mean(savings), mediansavings = median(savings), minsavings = min(savings), maxsavings = max(savings))
+ddply(trainingdata,.(gender),summarize, meansavings = mean(savings), mediansavings = median(savings), minsavings = min(savings), maxsavings = max(savings))
+#with(trainingdata,plot(income,savings))
+with(trainingdata,cor(income,savings))
+ddply(trainingdata,.(age),summarize, meancredit = mean(credit_amount), mediancredit = median(credit_amount), mincredit = min(credit_amount), maxcredit = max(credit_amount))
+ddply(trainingdata,.(children),summarize, meancredit = mean(credit_amount), mediancredit = median(credit_amount), mincredit = min(credit_amount), maxcredit = max(credit_amount))
+ddply(trainingdata,.(housing),summarize, meancredit = mean(credit_amount), mediancredit = median(credit_amount), mincredit = min(credit_amount), maxcredit = max(credit_amount))
+ddply(trainingdata,.(job),summarize, meancredit = mean(credit_amount), mediancredit = median(credit_amount), mincredit = min(credit_amount), maxcredit = max(credit_amount))
+# with(trainingdata,plot(income,credit_amount))
+# with(trainingdata,plot(savings,credit_amount))
+# with(trainingdata,plot(savings,credit_amount))
+ddply(trainingdata,.(default),summarize, meanincome = mean(income), minincome = min(income), maxincome = max(income))
+ddply(trainingdata,.(default),summarize, meansavings = mean(savings), mediansavings = median(savings), minsavings = min(savings), maxsavings = max(savings))
+ddply(trainingdata,.(default),summarize, meancredit = mean(credit_amount), mediancredit = median(credit_amount), mincredit = min(credit_amount), maxcredit = max(credit_amount))
+ddply(subset(trainingdata,job==2),.(default),summarize, meanlength = mean(length_of_employment), maxlength = max(length_of_employment))
+with(trainingdata,prop.table(table(education,default), margin = 1))
+with(trainingdata,prop.table(table(ethnicity,default), margin = 1))
+with(trainingdata,prop.table(table(age,default), margin = 1))
+
+
 
 returnlevels <- function(datatab) {
   factornames <- sapply(datatab,is.factor)
@@ -209,43 +260,47 @@ newdata <- credit$simulate(n=1000, return_simdata = TRUE)
 
 vnames <- credit$vnames
 modellist <- list(xgba_predict,xgbb_predict,xgbc_predict) 
-results <- matrix(NA, nrow = nrow(newdata), ncol = length(modellist))
+fairresults <- matrix(NA, nrow = nrow(newdata), ncol = length(modellist))
 for(i in 1:nrow(newdata)) {
   cat(i,date(),"\n")
   fairlist <- try(fairness( modellist = modellist,
            scm = credit, 
            sensitive = c("gender","ethnicity"),
            condition = newdata[i,..vnames], 
+           condition_type = list(age = "cont", 
+                                 ethnicity = "disc", 
+                                 marital = "disc", 
+                                 gender = "disc", 
+                                 education = "disc",
+                                 children = "disc",
+                                 job = "disc",
+                                 income = "cont",
+                                 housing = "disc",
+                                 address = "disc",
+                                 savings = "cont",
+                                 credit_history = "disc",
+                                 length_of_employment = "cont",
+                                 credit_amount = "cont",
+                                 default = "disc"
+           ),
            parents = setdiff(credit$pa("default",includeself = FALSE),
                              union( c("gender","ethnicity"), credit$unames)),
            n = 1000,
            modeltype = "function", 
-           control = list(method = "u_find",
-                          condition_type = list(age = "cont", 
-                                                ethnicity = "disc", 
-                                                marital = "disc", 
-                                                gender = "disc", 
-                                                education = "disc",
-                                                children = "disc",
-                                                job = "disc",
-                                                income = "cont",
-                                                housing = "disc",
-                                                address = "disc",
-                                                savings = "cont",
-                                                credit_history = "disc",
-                                                length_of_employment = "cont",
-                                                credit_amount = "cont",
-                                                default = "disc"
-                          ))
+           method = "u_find",
+           control = list(batchsize = 1000,
+                          maxbatchs = 1)
   ))
   if(!inherits(fairlist, "try-error")) {
     for(j in 1:length(modellist)) {
-      results[i,j] <- max(apply(fairlist[[j]],1,max) - apply(fairlist[[j]],1,min))
+      fairresults[i,j] <- max(apply(fairlist[[j]],1,max) - apply(fairlist[[j]],1,min))
     }
   }
 }
 
-#save(results, xgba, xgbb, xgbc, file = "credit_scoring_results.Rdata")
+save(fairresults, xgba, xgbb, xgbc, file = "credit_scoring_results_20240325.Rdata")
+
+print(summary(fairresults))
 
 latextable<-function(td) {
   tddim<-dim(td);
@@ -262,18 +317,17 @@ latextable<-function(td) {
 } 
 
 restable <- rbind(
-  apply(results,2,function(x) mean(x==0)),
-  apply(results,2,function(x) mean(x<0.01)),
-  apply(results,2,median),
-  apply(results,2,max)
-  )
+  apply(fairresults,2,function(x) mean(x==0, na.rm = TRUE)),
+  apply(fairresults,2,function(x) mean(x<0.01, na.rm = TRUE)),
+  apply(fairresults,2,median, na.rm = T),
+  apply(fairresults,2,max, na.rm = T)
+)
 
 restablet <- rbind(
-  c("Fairness %",formatC(100*apply(results,2,function(x) mean(x==0)), digits=0, format = "f")),
-  c("Difference <0.01 %",formatC(100*apply(results,2,function(x) mean(x<0.01)), digits=0, format = "f")),
-  c("Median difference",formatC(apply(results,2,median), digits=5, format = "f")),
-  c("Maximum difference",formatC(apply(results,2,max), digits=2, format = "f"))
+  c("Fairness %",formatC(100*apply(fairresults,2,function(x) mean(x==0, na.rm = TRUE)), digits=0, format = "f")),
+  c("Difference <0.01 %",formatC(100*apply(fairresults,2,function(x) mean(x<0.01, na.rm = TRUE)), digits=0, format = "f")),
+  c("Median difference",formatC(apply(fairresults,2,median, na.rm = TRUE), digits=5, format = "f")),
+  c("Maximum difference",formatC(apply(fairresults,2,max, na.rm = TRUE), digits=2, format = "f"))
 )
 latextable(restablet)
-
 
